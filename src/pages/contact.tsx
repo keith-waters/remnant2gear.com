423d05e1dc71d78003a6bb952ce8159ae0c6d1ea
keith-waters <readingwaters@gmail.com>
2024-01-16 21:31:58 -0500

    xxx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,13 +1,10 @@
 import { useState, MouseEventHandler } from 'react';
-import {  Typography, AppBar, Toolbar, IconButton, Menu, MenuItem } from "@mui/material";
+import {  Typography, AppBar, Toolbar, IconButton, Menu, MenuItem, Box } from "@mui/material";
 import theme from '../theme'
 import Link from 'next/link'
-import useMediaQuery from '@mui/material/useMediaQuery';
-import MenuIcon from '@mui/icons-material/Menu'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
 
-const R2GAppBar = ({openDrawer}:{openDrawer?:MouseEventHandler<HTMLButtonElement>}) => {
-  const showDesktopDrawer = useMediaQuery(theme.breakpoints.up('sm'));
+const R2GAppBar = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -17,28 +14,18 @@ const R2GAppBar = ({openDrawer}:{openDrawer?:MouseEventHandler<HTMLButtonElement
     setAnchorEl(null);
   };
   return (
-  <AppBar position="static" sx={{ paddingLeft: !showDesktopDrawer ? 'inherit' : theme.spacing(40)}}>
+  <AppBar position="static">
   <Toolbar>
-    {!showDesktopDrawer && 
-      <IconButton
-        size="large"
-        edge="start"
-        color="inherit"
-        aria-label="open drawer"
-        sx={{ mr: 2 }}
-        onClick={openDrawer}
+    <Link style={{ textDecoration: "none", color: theme.palette.text.primary }} href='/'>
+      <Typography
+        variant="h6"
+        noWrap
+        component="div"
       >
-        <MenuIcon />
-      </IconButton>
-    }
-    <Typography
-      variant="h6"
-      noWrap
-      component="div"
-      sx={{ flexGrow: 1,  }}
-    >
-      Remnant 2 Gear
-    </Typography>
+        Remnant 2 Gear
+      </Typography>
+    </Link>
+    <Box sx={{ flexGrow: 1 }}/>
     <IconButton
         onClick={handleClick}
       >
@@ -65,9 +52,29 @@ const R2GAppBar = ({openDrawer}:{openDrawer?:MouseEventHandler<HTMLButtonElement
   )
 }
 const Home = () => {
+  const imgUrl = '/android-chrome-192x192.png'
   return (
     <>
       <R2GAppBar />
+
+      <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', padding: theme.spacing(2), justifyContent: 'space-between', height: '500px' }}>
+        <img src={imgUrl} width={100}/>
+        <Typography variant='h4'>Thanks for checking out remnant2gear.com!</Typography>
+
+        <Typography>
+          This all started with me and two of my friends playing a lot of Remnant 2.
+        </Typography>
+        <Typography>
+          One of them was so into it, he made a spread sheet to help filter the gear and make builds.
+        </Typography>
+        <Typography>
+          And me being a web dev got excited about turning that spread sheet into this website!
+        </Typography>
+
+        <Typography>
+          If you have any questions, comments, or suggestions, reach out to my on twitter/X <a href='https://twitter.com/readingwaters'>@readingwaters</a> or on <a href='https://www.reddit.com/user/readingwaters'>reddit</a>!
+        </Typography>
+      </Box>
     </>
   );
 };
